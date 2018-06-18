e4ea873b84b46e23cf7f2ce2e8b15fca515810fb
Michael Jonathan Lee <code.mjlee@gmail.com>
2018-06-18 15:03:55 +0200

    Started slides on websockets.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
 
+projectName=$1
+
+outputDir=output/$projectName
+mkdir -p $outputDir
+
+xelatex -output-directory=$outputDir -halt-on-error -interaction=batchmode $projectName/slides.tex
